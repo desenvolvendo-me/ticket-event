e761e44581160c023f039749544e1bccab5f068a
Marco Castro <marcodotcastro@gmail.com>
2023-08-17 15:25:45 -0300

    feat: build ticket csv

--- app/businesses/tickets/build.rb
+++ app/businesses/tickets/build.rb
@@ -1,4 +1,6 @@
 require 'nokogiri'
+require 'csv'
+require 'mini_magick'
 
 module Tickets
   class Build < BusinessApplication
@@ -10,23 +12,38 @@ module Tickets
 
     def call
       svg_file_path = @params[:template_path]
-      svg_copy_path = 'spec/support/ticket-changed.svg'
 
-      svg_content = File.open(svg_file_path) { |f| Nokogiri::XML(f) }
+      CSV.foreach(@params[:csv_path], headers: true) do |row|
+        svg_content = File.open(svg_file_path) { |f| Nokogiri::XML(f) }
 
-      replacements = {
-        "NOME" => "MARCO CASTRO",
-        "EMAIL" => "marcodotcastro@gmail.com"
-      }
+        replacements = {
+          "NOME" => first_and_last_name(row)
+        }
 
-      svg_content.css("//text").each do |text_element|
-        text_content = text_element.content
-        new_content = text_content.dup
-        replacements.each { |search_text, replacement| new_content.gsub!(search_text, replacement) }
-        text_element.content = new_content
+        svg_copy_path = "spec/support/ticket-changed-#{row['phone'] }"
+
+        svg_content.css("//text").each do |text_element|
+          text_content = text_element.content
+          new_content = text_content.dup
+          replacements.each { |search_text, replacement| new_content.gsub!(search_text, replacement) }
+          text_element.content = new_content
+        end
+
+        File.open(svg_copy_path + ".svg", 'w') { |file| file.write(svg_content.to_xml) }
+
+        image = MiniMagick::Image.open(svg_copy_path + ".svg")
+
+        image.format 'png'
+        image.write svg_copy_path + ".png"
       end
 
-      File.open(svg_copy_path, 'w') { |file| file.write(svg_content.to_xml) }
+    end
+
+    private
+
+    def first_and_last_name(row)
+      array = row['fullname'].split
+      array.first + " " + array.last
     end
 
   end
