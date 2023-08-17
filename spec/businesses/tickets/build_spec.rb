5db524b62f9d55905e8d8a4ceb8a1f4eab1c0898
Marco Castro <marcodotcastro@gmail.com>
2023-08-17 15:25:55 -0300

    test: build ticket csv

--- spec/businesses/tickets/build_spec.rb
+++ spec/businesses/tickets/build_spec.rb
@@ -3,12 +3,15 @@ require 'rails_helper'
 RSpec.describe Tickets::Build do
 
   it 'build' do
-    ticket_changed = "spec/support/ticket-changed.svg"
-    ticket_success = "spec/support/ticket-success.svg"
+    csv_path = 'spec/support/leads_export.csv'
+    template_path = "spec/support/ticket.svg"
 
-    Tickets::Build.call({ template_path: "spec/support/ticket.svg" })
+    Tickets::Build.call({ template_path: template_path, csv_path: csv_path })
 
-    expect(File.read(ticket_changed)).to eq(File.read(ticket_success))
+    expect(File.exist?("spec/support/ticket-changed-61991707479.svg")).to be(true)
+    expect(File.exist?("spec/support/ticket-changed-61991707479.png")).to be(true)
+    expect(File.exist?("spec/support/ticket-changed-5561991707479.svg")).to be(true)
+    expect(File.exist?("spec/support/ticket-changed-5561991707479.png")).to be(true)
   end
 
 end
