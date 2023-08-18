1addd93a220e38469d3d92e8208fe22ce67a77d4
Marco Castro <marcodotcastro@gmail.com>
2023-08-18 10:58:10 -0300

    feat: create student

---
+++ app/models/student.rb
@@ -0,0 +1,14 @@
+# == Schema Information
+#
+# Table name: students
+#
+#  id         :bigint           not null, primary key
+#  email      :string
+#  name       :string
+#  phone      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Student < ApplicationRecord
+  has_many :tickets
+end
