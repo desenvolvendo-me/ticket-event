41d597cba8ed4f2bc0b4a56c1217946a116d26a6
Marco Castro <marcodotcastro@gmail.com>
2023-08-22 09:35:49 -0300

    build: i18n activeadmin

--- app/admin/tickets.rb
+++ app/admin/tickets.rb
@@ -38,7 +38,7 @@ ActiveAdmin.register Ticket do
   end
 
   action_item :upload_csv, :only => :index do
-    link_to "Importar CSV", :action => 'upload_csv'
+    link_to t("active_admin.actions.upload_csv"), :action => 'upload_csv'
   end
 
   collection_action :upload_csv, only: :index do
@@ -48,27 +48,27 @@ ActiveAdmin.register Ticket do
   collection_action :import_csv, :method => :post do
     event = Event.find(params[:ticket][:event_id])
     Tickets::Builder.call({ event: event, csv_path: params["ticket"]["file"] })
-    redirect_to :action => :index, :notice => "CSV Enviado para Importação com Sucesso!"
+    redirect_to :action => :index, :notice => t("active_admin.notice.ticket.import_csv")
   end
 
   action_item :send_email, only: :show do
-    link_to "Enviar Email", send_email_admin_ticket_path(resource), method: :put
+    link_to t("active_admin.actions.send_email"), send_email_admin_ticket_path(resource), method: :put
   end
 
   member_action :send_email, method: :put do
     Notifications::Notifier.call(ticket: resource)
 
-    redirect_to admin_ticket_path(resource), :notice => "Email Enviado!"
+    redirect_to admin_ticket_path(resource), :notice => t("active_admin.notice.ticket.email")
   end
 
   action_item :send_emails, only: :index do
-    link_to "Enviar Emails", send_emails_admin_tickets_path, method: :post
+    link_to t("active_admin.actions.send_emails"), send_emails_admin_tickets_path, method: :post
   end
 
   collection_action :send_emails, method: :post do
     Notifications::Notifier.call
 
-    redirect_to admin_tickets_path, :notice => "Solicitação de Emails Enviado!"
+    redirect_to admin_tickets_path, :notice => t("active_admin.notice.ticket.emails")
   end
 
 end
