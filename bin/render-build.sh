4849e066958bfb17df7c894af4a9027ccb3e0578
Marco Castro <marcodotcastro@gmail.com>
2023-08-22 13:14:18 -0300

    build: configuration render

---
+++ bin/render-build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+npm install esbuild
+
+bundle install
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rake db:migrate
+
+bin/rails tailwindcss:build
