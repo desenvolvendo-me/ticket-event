9d7c7340f85528618d4be7c4ab602e651df50b88
Marco Castro <marcodotcastro@gmail.com>
2023-08-22 09:14:58 -0300

    refactor: email ticket

--- app/mailers/ticket_to_event_mailer.rb
+++ app/mailers/ticket_to_event_mailer.rb
@@ -2,6 +2,6 @@ class TicketToEventMailer < ApplicationMailer
 
   def send_ticket
     @ticket = params[:ticket]
-    mail(to: @ticket.student.email, subject: 'Ingresso para o Evento')
+    mail(to: @ticket.student.email, subject: "Seu Ingresso para o Evento: #{@ticket.event.name}")
   end
 end
