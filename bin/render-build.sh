861cd480b0b327254831e1e1ea4ffe0374dee459
Marco Castro <marcodotcastro@gmail.com>
2023-08-23 16:10:23 -0300

    build: render fonts

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,11 +2,17 @@
 # exit on error
 set -o errexit
 
-npm install esbuild
+### Install Fonts MiniMagick
+mkdir ~/.fonts
+cp .render/fonts/* ~/.fonts
+fc-cache -f -v
 
+### Configurations Rails
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
 
+### Configurations Tailwind
+npm install esbuild
 bin/rails tailwindcss:build
