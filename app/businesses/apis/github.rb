36027c15e4b7786c3ec442770ca4ecfe4fde0927
Marco Castro <marcodotcastro@gmail.com>
2023-08-29 10:49:55 -0300

    build: ci/cd test

--- app/businesses/apis/github.rb
+++ app/businesses/apis/github.rb
@@ -11,9 +11,11 @@ module Apis
       results = HTTParty.get("https://api.github.com/users/#{@student.profile_social.delete("@")}", :headers => {
         "Accept" => "application/vnd.github+json",
         "X-GitHub-Api-Version" => "2022-11-28",
-        "Authorization" => "Bearer #{ENV['GITHUB_KEY']}"
+        "Authorization" => "Bearer #{ENV['API_GITHUB_KEY']}"
       })
       results["avatar_url"]
+    rescue
+      nil
     end
   end
 end
