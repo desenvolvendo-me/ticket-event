b0af10ae80522c080708e72143969d77e9ab3c82
Philipe <dev.philipe@outlook.com>
2023-08-30 15:30:06 -0300

    test(lessons): initial configuration

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -37,6 +37,8 @@ RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
+  # Config to run rspec request tests with sign in
+  config.include Devise::Test::IntegrationHelpers, type: :request
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
