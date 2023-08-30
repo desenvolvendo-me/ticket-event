0a97a8ac8133dceb403195cd622559664c8f90b4
Philipe <dev.philipe@outlook.com>
2023-08-30 15:27:59 -0300

    feat(Lessons): lessons created and linked to event

--- app/models/event.rb
+++ app/models/event.rb
@@ -20,6 +20,7 @@ class Event < ApplicationRecord
   friendly_id :launch_and_name, use: :slugged
 
   has_many :tickets, dependent: :destroy
+  has_many :lessons
 
   validates :name, :launch, presence: true
 
---
+++ db/migrate/20230830173115_create_lessons.rb
@@ -0,0 +1,10 @@
+class CreateLessons < ActiveRecord::Migration[7.0]
+  def change
+    create_table :lessons do |t|
+      t.string :link
+      t.references :event, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
