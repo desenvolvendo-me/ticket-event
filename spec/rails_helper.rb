93961e4cb9afcabebf481e17bf44883730d29b4e
Philipe <dev.philipe@outlook.com>
2023-08-31 19:07:51 -0300

    test(lessons): tests removed and config restarted

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -36,9 +36,6 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
-  # Config to run rspec request tests with sign in
-  config.include Devise::Test::IntegrationHelpers, type: :request
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
