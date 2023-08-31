60881a0139f763114c7faca0e05f3784dc487c79
Philipe <dev.philipe@outlook.com>
2023-08-31 19:05:23 -0300

    fix(lessons): comments removed

--- app/admin/lessons.rb
+++ app/admin/lessons.rb
@@ -1,10 +1,4 @@
 ActiveAdmin.register Lesson do
-
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
   permit_params :link, :event_id
 
   index do
@@ -26,13 +20,4 @@ ActiveAdmin.register Lesson do
       end
     end
   end
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:link, :event_id]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
 end
