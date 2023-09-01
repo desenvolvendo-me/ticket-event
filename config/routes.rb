ab82ab1380693438605d0e5b4d079b0499565da7
Cassio Santana <cassioo.roger@gmail.com>
2023-09-01 17:11:41 -0300

    feat(certificate): Configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,10 @@ Rails.application.routes.draw do
   get ':slug_event/ticket/:phone/edit', to: "tickets#edit", as: :edit_event_ticket
   put ':slug_event/ticket/:phone/update', to: "tickets#update", as: :update_event_ticket
 
+  get ':slug_event/certificate/:phone', to: "certificates#certificate", as: :event_certificate
+  get ':slug_event/certificate', to: "certificates#search", as: :search_certificate
+  post 'certificate/form', to: "certificates#form", as: :form_certificate
+
   get ':slug_event/checkin', to: "checkins#search", as: :search_checkin
   post 'checkin/form', to: "checkins#form", as: :form_checkin
   get ':slug_event/checked/:phone', to: "checkins#checked", as: :checked_checkin
