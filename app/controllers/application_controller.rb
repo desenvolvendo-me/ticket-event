ba41f26d9cdc78a731b2afe7f724e0895d57afe7
Marco Castro <marcodotcastro@gmail.com>
2023-09-01 12:13:20 -0300

    refactor: remove authenticator

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,3 @@
 class ApplicationController < ActionController::Base
   include ActiveStorage::SetCurrent
-  # TODO: Devise já configurado para o frontend,
-  # mais ainda não precisa ser usado
-  # before_action :authenticate_user!
 end
--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,5 +1,4 @@
 class TicketsController < ApplicationController
-  # skip_before_action :authenticate_user!, :only => [:ticket, :search, :form, :edit, :update]
   layout "external"
 
   before_action :get_ticket, :only => [:ticket, :form, :update]
