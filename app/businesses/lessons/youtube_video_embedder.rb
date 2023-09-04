42766d74f772796be625be462c30c85d714f429a
Philipe <dev.philipe@outlook.com>
2023-09-04 17:17:30 -0300

    feat(YoutubeVideoEmbedder): extract more kinds of youtube url videos

--- app/businesses/lessons/youtube_video_embedder.rb
+++ app/businesses/lessons/youtube_video_embedder.rb
@@ -2,10 +2,25 @@ module Lessons
   class YoutubeVideoEmbedder
     def self.embed_url(video_url)
       uri = URI.parse(video_url)
-      video_id = uri.path.split('/').last
+      video_id = extract_video_id(uri)
       iframe_url = "https://www.youtube.com/embed/#{video_id}"
       iframe_url
     end
+
+    private
+
+    def self.extract_video_id(uri)
+      if uri.path.include?('/v/')
+        return uri.path.split('/v/').last
+      elsif uri.query && uri.query.include?('v=')
+        params = URI.decode_www_form(uri.query)
+        video_id = params.assoc('v').last
+        return video_id
+      else
+        raise ArgumentError, 'URL do YouTube em formato desconhecido ou inválido.'
+      end
+    end
   end
 end
+
 
