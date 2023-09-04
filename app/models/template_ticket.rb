d574332c06c239c181d177cd8c2069f18e2ca055
Philipe <dev.philipe@outlook.com>
2023-09-04 16:41:58 -0300

    fix(lessons): comments removed or changed

--- app/models/template_ticket.rb
+++ app/models/template_ticket.rb
@@ -2,9 +2,12 @@
 #
 # Table name: template_tickets
 #
-#  id         :bigint           not null, primary key
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id          :bigint           not null, primary key
+#  description :string
+#  name        :string
+#  version     :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
 #
 class TemplateTicket < ApplicationRecord
   has_one_attached :svg
