398070af1fe8bd5a18253c6345e6a673046ae0fc
Philipe <dev.philipe@outlook.com>
2023-09-04 19:33:07 -0300

    fix(Embedder): name fixed on the whole scope

--- spec/businesses/lessons/embedder_spec.rb
+++ spec/businesses/lessons/embedder_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe Lessons::YoutubeVideoEmbedder, type: :lib do
+RSpec.describe Lessons::Embedder, type: :lib do
   describe '.embed_url' do
     it 'returns the embedded YouTube video URL' do
       video_url = 'https://www.youtube.com/watch?v=3qgcPXnmbA8&ab_channel=JacksonPires'
-      embedded_url = Lessons::YoutubeVideoEmbedder.embed_url(video_url)
+      embedded_url = Lessons::Embedder.embed_url(video_url)
       expected_url = 'https://www.youtube.com/embed/3qgcPXnmbA8'
 
       expect(embedded_url).to eq(expected_url)
@@ -14,8 +14,8 @@ RSpec.describe Lessons::YoutubeVideoEmbedder, type: :lib do
       video_url1 = 'https://www.youtube.com/watch?v=dWVo2Ma6ZpU&t=5s&ab_channel=ProgramadorShowzim'
       video_url2 = 'https://youtube.com/v/dWVo2Ma6ZpU&t=5s'
 
-      embedded_url1 = Lessons::YoutubeVideoEmbedder.embed_url(video_url1)
-      embedded_url2 = Lessons::YoutubeVideoEmbedder.embed_url(video_url2)
+      embedded_url1 = Lessons::Embedder.embed_url(video_url1)
+      embedded_url2 = Lessons::Embedder.embed_url(video_url2)
 
       expected_url1 = 'https://www.youtube.com/embed/dWVo2Ma6ZpU'
       expected_url2 = 'https://www.youtube.com/embed/dWVo2Ma6ZpU&t=5s'
