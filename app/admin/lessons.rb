f255a30b2dc2f2e836d5deb01de2adeac06010b2
Philipe <dev.philipe@outlook.com>
2023-09-04 16:44:49 -0300

    feat(lessons): more fields created

--- app/admin/lessons.rb
+++ app/admin/lessons.rb
@@ -1,10 +1,12 @@
 ActiveAdmin.register Lesson do
-  permit_params :link, :event_id
+  permit_params :link, :event_id, :title, :description
 
   index do
     selectable_column
     id_column
     column :event
+    column :title
+    column :description
     column :link do |lesson|
       link_to lesson.link, lesson.link, target: '_blank'
     end
@@ -15,9 +17,21 @@ ActiveAdmin.register Lesson do
     attributes_table do
       row :id
       row :event
+      row :title
+      row :description
       row :link do |lesson|
         link_to lesson.link, lesson.link, target: '_blank'
       end
     end
   end
+  form do |f|
+    f.inputs "Lesson Details" do
+      f.input :event
+      f.input :link
+      f.input :title
+      f.input :description
+    end
+    f.actions
+  end
+
 end
