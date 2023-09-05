7d3558d8838353c2e64d21b7d53635968f36f33f
Marco Castro <marcodotcastro@gmail.com>
2023-09-05 12:09:40 -0300

    chore: remove link

--- spec/factories/lessons.rb
+++ spec/factories/lessons.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: lessons
+#
+#  id          :bigint           not null, primary key
+#  description :text
+#  link        :string
+#  title       :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  event_id    :bigint           not null
+#
+# Indexes
+#
+#  index_lessons_on_event_id  (event_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (event_id => events.id)
+#
 FactoryBot.define do
   factory :lesson do
     title { Faker::Lorem.sentence }
