8422165d4bfffdf493c47ca8b0da2558f07aa729
PhilipeeX <dev.philipe@outlook.com>
2023-09-05 19:08:10 -0300

    feat(lesson): launch_datetime and thumbnail fields added

--- app/admin/lessons.rb
+++ app/admin/lessons.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Lesson do
-  permit_params :link, :event_id, :title, :description
+  permit_params :link, :event_id, :title, :description, :launch_datetime,:thumbnail
 
   index do
     selectable_column
@@ -7,6 +7,7 @@ ActiveAdmin.register Lesson do
     column :event
     column :title
     column :description
+    column :launch_datetime
     column :link do |lesson|
       link_to lesson.link, lesson.link, target: '_blank'
     end
@@ -19,17 +20,24 @@ ActiveAdmin.register Lesson do
       row :event
       row :title
       row :description
+      row :launch_datetime
       row :link do |lesson|
         link_to lesson.link, lesson.link, target: '_blank'
       end
+      row :thumbnail do |lesson|
+        image_tag lesson.thumbnail, style: 'max-width: 400px;' if lesson.thumbnail.attached?
+      end
     end
   end
+
   form do |f|
-    f.inputs "Lesson Details" do
+    f.inputs  I18n.t('lesson.lesson_details') do
       f.input :event
       f.input :link
       f.input :title
       f.input :description
+      f.input :launch_datetime
+      f.input :thumbnail, as: :file
     end
     f.actions
   end
