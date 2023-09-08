2cc38f9b8004f930d89ab3704732d7d5dd8ded2d
Cassio Santana <cassioo.roger@gmail.com>
2023-09-08 13:03:53 -0300

    chore(certificate): add translation to certificate creation views

--- app/admin/students.rb
+++ app/admin/students.rb
@@ -2,10 +2,10 @@ ActiveAdmin.register Student do
   permit_params :name, :email, :phone
 
   action_item :select_event, only: :show do
-    link_to "Gerar certificado", action: "select_event", id: resource.id
+    link_to t("active_admin.actions.generate_certificate"), action: "select_event", id: resource.id
   end
 
-  collection_action :select_event, title: "Gerar certificado", only: :show do
+  collection_action :select_event, title: I18n.t("active_admin.actions.generate_certificate"), only: :show do
 
   end
 
@@ -15,6 +15,6 @@ ActiveAdmin.register Student do
     certificate = Certificate.create(student_id: student.id, event_id: event.id)
     Certificates::Builder.call(certificate: certificate)
 
-    redirect_to action: :index, notice: "Certificado gerado com sucesso!"
+    redirect_to action: :index, notice: t("active_admin.notice.student.certificate_generated_successfully")
   end
 end
