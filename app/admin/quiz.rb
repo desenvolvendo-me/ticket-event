3bf64c70b0590e43bc3017ba983f559ead63e15b
Philipe <dev.philipe@outlook.com>
2023-09-18 11:05:01 -0300

    chore(I18n): fix some lesson and quizzes translations

--- app/admin/quiz.rb
+++ app/admin/quiz.rb
@@ -28,12 +28,12 @@ ActiveAdmin.register Quiz do
 
     f.inputs I18n.t('active_admin.quiz.questions.title') do
       f.has_many :quiz_questions, allow_destroy: true, heading: I18n.t('active_admin.quiz.questions.title') do |question|
-        question.input :description, label: 'Descrição da pergunta'
-        question.input :answer1, label: 'Resposta 1'
-        question.input :answer2, label: 'Resposta 2'
-        question.input :answer3, label: 'Resposta 3'
-        question.input :answer4, label: 'Resposta 4'
-        question.input :correct_answer, label: 'Resposta correta', as: :select, collection: [[I18n.t('active_admin.quiz.quiz_details.answer1'), 1], [I18n.t('active_admin.quiz.quiz_details.answer2'), 2], [I18n.t('active_admin.quiz.quiz_details.answer3'), 3], [I18n.t('active_admin.quiz.quiz_details.answer4'), 4]], include_blank: false
+        question.input :description, label: I18n.t('active_admin.quiz.quiz_details.question_description')
+        question.input :answer1, label: I18n.t('active_admin.quiz.quiz_details.answer1')
+        question.input :answer2, label: I18n.t('active_admin.quiz.quiz_details.answer2')
+        question.input :answer3, label: I18n.t('active_admin.quiz.quiz_details.answer3')
+        question.input :answer4, label: I18n.t('active_admin.quiz.quiz_details.answer4')
+        question.input :correct_answer, label: I18n.t('active_admin.quiz.quiz_details.correct_answer'), as: :select, collection: [[I18n.t('active_admin.quiz.quiz_details.answer1'), 1], [I18n.t('active_admin.quiz.quiz_details.answer2'), 2], [I18n.t('active_admin.quiz.quiz_details.answer3'), 3], [I18n.t('active_admin.quiz.quiz_details.answer4'), 4]], include_blank: false
       end
     end
 
