03dc25fb96a55cb1b36326f6f1b715ef784b5241
Philipe <dev.philipe@outlook.com>
2023-09-18 10:48:00 -0300

    feat(quiz): actions submit and result created

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -6,7 +6,41 @@ class QuizController < ExternalController
   end
 
   def submit
+    @lesson = Lesson.find(params[:lesson_id])
+    @quiz = @lesson.quiz
+    @quiz_questions = @quiz.quiz_questions
+    @student_responses = params[:responses]
+
+    correct_responses = {}
+    incorrect_responses = {}
+
+    @quiz_questions.each do |question|
+      correct_answer = question.correct_answer
+      student_answer = @student_responses[question.id.to_s].sub("answer", "").to_i
+
+      if student_answer == correct_answer
+        correct_responses[question.id] = student_answer
+      else
+        incorrect_responses[question.id] = {
+          student_answer: student_answer,
+          correct_answer: correct_answer,
+        }
+      end
+    end
+
+    redirect_to quiz_result_path(correct_responses: correct_responses, incorrect_responses: incorrect_responses)
   end
+
+
+  def result
+    @lesson = Lesson.find(params[:lesson_id])
+    @quiz = @lesson.quiz
+    @quiz_questions = @quiz.quiz_questions
+    @correct_responses = params[:correct_responses]
+    @incorrect_responses = params[:incorrect_responses]
+  end
+
+
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -25,4 +25,5 @@ Rails.application.routes.draw do
   get ':slug_event/lessons', to: "lessons#index"
   get ':slug_event/lessons/:lesson_id/quiz', to: "quiz#show", as: :quiz
   post ':slug_event/lessons/:lesson_id/quiz/submit', to: "quiz#submit", as: :quiz_submit
+  get ':slug_event/lessons/:lesson_id/quiz/result', to: "quiz#result", as: :quiz_result
 end
