93f7849d2fe145bfaf172363122c8c635970d392
Marco Castro <marcodotcastro@gmail.com>
2023-09-19 12:24:59 -0300

    refactor: lesson validator

--- app/businesses/lessons/validator.rb
+++ app/businesses/lessons/validator.rb
@@ -3,15 +3,15 @@ module Lessons
     def initialize(lesson)
       @lesson = lesson
     end
+
     def call
       validate_max_lessons_per_event
     end
 
     private
+
     def validate_max_lessons_per_event
-      if @lesson.event.lessons.count >= 5
-        @lesson.errors.add(:event, I18n.t("businesses.lesson.validator.max_lessons") + "#{@lesson.event.name}")
-      end
+      I18n.t("businesses.lesson.validator.max_lessons") + "#{@lesson.event.name}" if @lesson.event.lessons.count >= 5
     end
   end
 end
