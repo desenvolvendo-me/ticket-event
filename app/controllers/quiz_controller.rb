d0ba56448ae59ab494bcf5b6e7a6ff697ee30f8d
Philipe Rodrigues <dev.philipe@outlook.com>
2023-09-20 11:04:04 -0300

    feat: quiz authentication by phone

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -1,10 +1,22 @@
 class QuizController < ExternalController
+  before_action :get_ticket, only: %i[ form ]
   def show
     @lesson = Lesson.find(params[:lesson_id])
     @quiz = @lesson.quiz
     @quiz_questions = @quiz.quiz_questions
   end
 
+  def search
+  end
+
+  def form
+    if @ticket
+      @lesson = Lesson.find(params[:lesson_id])
+      redirect_to quiz_path(@lesson, @lesson.quiz)
+    else
+      redirect_to search_ticket_path(params["slug_event"]), notice: "O número não foi cadastrado nesse evento!"
+    end
+  end
   def submit
     @lesson = Lesson.find(params[:lesson_id])
     @quiz = @lesson.quiz
@@ -38,5 +50,12 @@ class QuizController < ExternalController
     @quiz_questions = @quiz.quiz_questions
     @correct_responses = params[:correct_responses]
     @incorrect_responses = params[:incorrect_responses]
+  end
+
+  private
+
+  def get_ticket
+    @ticket = Ticket.joins(:event, :student)
+                    .where(events: { slug: params["slug_event"] }, students: { phone: params["phone"] }).take
   end
 end
