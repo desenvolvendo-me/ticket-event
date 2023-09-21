6eafb8ab7375ff1c6397b7223d5214d82851976d
Cassio Santana <cassioo.roger@gmail.com>
2023-09-21 15:20:23 -0300

    refactor(prize_draw): Change dependency creation

--- app/models/prize_draw.rb
+++ app/models/prize_draw.rb
@@ -5,20 +5,16 @@
 #  id         :bigint           not null, primary key
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
-#  event_id   :bigint           not null
-#  winner_id  :bigint           not null
+#  ticket_id  :bigint           not null
 #
 # Indexes
 #
-#  index_prize_draws_on_event_id   (event_id)
-#  index_prize_draws_on_winner_id  (winner_id)
+#  index_prize_draws_on_ticket_id  (ticket_id)
 #
 # Foreign Keys
 #
-#  fk_rails_...  (event_id => events.id)
-#  fk_rails_...  (winner_id => students.id)
+#  fk_rails_...  (ticket_id => tickets.id)
 #
 class PrizeDraw < ApplicationRecord
-  belongs_to :event
-  belongs_to :winner, class_name: "Student"
+  belongs_to :ticket
 end
