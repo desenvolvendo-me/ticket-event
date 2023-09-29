4ad81fe81c8347d43aaf68dd235292013f5133bd
ricardofelipe7 <rikrdoofelipe@outlook.com>
2023-09-29 17:00:09 -0300

    refactor(events_controller): adding functions for views

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,12 @@
 class EventsController < ApplicationController
+  before_action :get_event, only: %i[ index select_event ]
   def index
+  end
+
+  def select_event; end
+
+  private
+  def get_event
+    @event_day = Event.friendly.find_by(params[:launch_and_name])
   end
 end
