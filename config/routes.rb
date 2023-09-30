69b8d3ce364a77e646056ef615e402c0adb74134
ricardofelipe7 <rikrdoofelipe@outlook.com>
2023-09-30 11:33:40 -0300

    refactor(route): change event view

--- config/routes.rb
+++ config/routes.rb
@@ -27,5 +27,6 @@ Rails.application.routes.draw do
   post ':slug_event/lessons/:lesson_id/quiz/submit', to: "quiz#submit", as: :quiz_submit
   get ':slug_event/lessons/:lesson_id/quiz/result', to: "quiz#result", as: :quiz_result
 
-  get ':slug_event/events', to: "events#index"
+  get ':slug_event/events', to: "events#index", as: :events
+  get ':slug_event/events/id', to: "events#event_detail", as: :event_detail
 end
