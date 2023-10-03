2194ba8197fe5b20f5acb538dd16b134e27fe12b
Philipe Rodrigues <dev.philipe@outlook.com>
2023-10-03 09:17:38 -0300

    build(database): change ticket entity database fields

--- db/migrate/20230921170941_add_student_score_to_ticket.rb
+++ db/migrate/20230921170941_add_student_score_to_ticket.rb
@@ -1,5 +1,5 @@
 class AddStudentScoreToTicket < ActiveRecord::Migration[7.0]
   def change
-    add_column :tickets, :student_score, :json
+    add_column :tickets, :student_score, :integer
   end
 end
