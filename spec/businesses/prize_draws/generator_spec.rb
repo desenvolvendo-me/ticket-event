c427c15276ca1378e9f08da59fdddd0e59afde04
Cassio Santana <cassioo.roger@gmail.com>
2023-10-06 05:58:57 -0300

    test(prize_draw): Increase number of students and add score variety in tests

--- spec/businesses/prize_draws/generator_spec.rb
+++ spec/businesses/prize_draws/generator_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe PrizeDraws::Generator do
   let(:event) { create(:event) }
   let(:students) do
-    Array.new(3) do
+    Array.new(5) do
       create(:student, name: FFaker::Name.name)
     end
   end
@@ -12,11 +12,19 @@ RSpec.describe PrizeDraws::Generator do
   context 'when students achieve the required score' do
     it "the draw is carried out successfully" do
 
-      students.each do |student|
+      high_score_students = students.sample(2)
+      low_score_students = students - high_score_students
+
+      high_score_students.each do |student|
         create(:ticket, student: student, event: event, student_score: rand(70..100))
       end
 
+      low_score_students.each do |student|
+        create(:ticket, student: student, event: event, student_score: rand(0..69))
+      end
+
       expect(event.tickets).to include(prize_draw.ticket)
+      expect(high_score_students).to include(prize_draw.ticket.student)
       expect(prize_draw.ticket.student_score).to be >= 70
     end
   end
