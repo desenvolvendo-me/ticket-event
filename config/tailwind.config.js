d08dad2c5530d1d7bdc40ae576cff480db8c6501
Philipe Rodrigues <dev.philipe@outlook.com>
2023-10-07 10:40:50 -0300

    build(flowbite): configured by CDN directly on view page

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -12,6 +12,21 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        primary: {
+          "50": "#eff6ff",
+          "100": "#dbeafe",
+          "200": "#bfdbfe",
+          "300": "#93c5fd",
+          "400": "#60a5fa",
+          "500": "#3b82f6",
+          "600": "#2563eb",
+          "700": "#1d4ed8",
+          "800": "#1e40af",
+          "900": "#1e3a8a",
+          "950": "#172554",
+        },
+      },
     },
   },
   plugins: [
@@ -19,5 +34,6 @@ module.exports = {
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/typography'),
     require('@tailwindcss/container-queries'),
-  ]
+  ],
+  darkMode: 'class',
 }
