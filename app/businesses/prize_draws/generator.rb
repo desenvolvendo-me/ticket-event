e4a1c0d65140ead63978e5e2ae049ccc0aab86a3
Cassio Santana <cassioo.roger@gmail.com>
2023-10-06 23:07:42 -0300

    refactor(prize_draw): add constant for passing score and check for prize draw creation

--- app/businesses/prize_draws/generator.rb
+++ app/businesses/prize_draws/generator.rb
@@ -1,18 +1,21 @@
 module PrizeDraws
   class Generator < BusinessApplication
+
+    PASSING_SCORE = 70
+
     def initialize(event)
       @event = event
     end
 
     def call
       draw_ticket
-      create_prize_draw
+      create_prize_draw if @drawn_ticket
     end
 
     private
 
     def draw_ticket
-      @drawn_ticket = @event.tickets.where("student_score >= ?", 70).sample
+      @drawn_ticket = @event.tickets.where("student_score >= ?", PASSING_SCORE).sample
     end
 
     def create_prize_draw
