7305db6cfb8308359c806f6a95ddf8425bfa1e63
ricardofelipe7 <rikrdoofelipe@outlook.com>
2023-10-11 09:58:18 -0300

    feat: create controller manager::events

---
+++ app/controllers/manager/events_controller.rb
@@ -0,0 +1,19 @@
+class Manager::EventsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
