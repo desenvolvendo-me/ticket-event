62209f66cc11d1aeb92e962d9b1d6f6356dc116e
Cassio Santana <cassioo.roger@gmail.com>
2023-10-16 19:04:57 -0300

    feat(student): Add students edit to manager

--- app/controllers/manager/students_controller.rb
+++ app/controllers/manager/students_controller.rb
@@ -1,6 +1,6 @@
 module Manager
   class StudentsController < ApplicationController
-    before_action :set_student, only: %i[show]
+    before_action :set_student, only: %i[show edit update]
 
     def index
       @students = Student.all
@@ -12,12 +12,20 @@ module Manager
       @student = Student.new
     end
 
+    def edit; end
+
     def create
       @student = Student.new(student_params)
 
       return render :new unless @student.save
 
       redirect_to manager_students_path, notice: 'Student was successfully created.'
+    end
+
+    def update
+      return render :edit, status: :unprocessable_entity unless @student.update(student_params)
+
+      redirect_to manager_student_path(@student), notice: "Student was successfully updated."
     end
 
     private
