d8495b7594df6f92bd76fd23453fbce93758168f
ricardofelipe7 <rikrdoofelipe@outlook.com>
2023-10-16 14:37:31 -0300

    feat(controller): created ticket.

---
+++ app/controllers/manager/tickets_controller.rb
@@ -0,0 +1,39 @@
+class Manager::TicketsController < ApplicationController
+  before_action :set_event_options, only: [:create, :new]
+  before_action :set_ticket, only: [:create, :new]
+
+  def index
+    @tickets = Ticket.all
+  end
+
+  def new
+    @ticket = Ticket.new
+  end
+
+  def create
+    @ticket = Ticket.new(ticket_params)
+
+    if @ticket.save
+      redirect_to manager_tickets_path(@ticket)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_ticket
+  # @ticket = Ticket.joins(:event, :student)
+  #           .where(events: { slug: params["slug_event"] }, students: { phone: params["phone"] }).take
+
+    @ticket = Ticket.find_by(params[:id])
+  end
+
+  def ticket_params
+    params.require(:ticket).permit(:event_id, :student_id)
+  end
+
+  def set_event_options
+    @event_options = Event.all.map { |e| [e.id, e.name] }
+  end
+end
