8ec0f1c82f51bd0bdb6e8309844ebdeca474d17f
ricardofelipe7 <rikrdoofelipe@outlook.com>
2023-10-17 19:06:38 -0300

    test(controller): Refactoring test post and add test show.

--- spec/controllers/manager/tickets_spec.rb
+++ spec/controllers/manager/tickets_spec.rb
@@ -8,21 +8,30 @@ RSpec.describe Manager::TicketsController, type: :controller do
     end
   end
 
-  describe 'GET /new' do
+  describe 'GET /show' do
     it 'should returns a successful response' do
-      get :new
+      ticket = create(:ticket)
+      get :show, params: { id: ticket.id }
+      expect(response).to be_successful
+    end
+    end
+
+  describe 'GET /select_student_csv' do
+    it 'should returns a successful response' do
+      get :select_student_csv
       expect(response).to be_successful
     end
   end
 
-  describe 'POST /create' do
-    it 'should returns a successful response' do
+  describe 'POST /import_student_csv' do
+    it 'should generate ticket and redirects to the index' do
       event = create(:event)
-      student = create(:student)
-
       expect {
-        post :create, params: { ticket: {event_id: event.id, student_id: student.id}}
+        post :import_student_csv, params: { ticket: { event_id: event.id, file: "spec/support/leads_export.csv" } }
       }.to change(Ticket, :count).by(1)
+
+      expect(response).to redirect_to(action: :index)
     end
   end
+
 end
