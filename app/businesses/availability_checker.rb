0b5e522605dc0dceb16f23abbf970a229ab75df8
ricardofelipe7 <rikrdoofelipe@outlook.com>
2023-10-24 15:43:18 -0300

    refactor(event availability): switch availability logic to business layer

--- app/businesses/availability_checker.rb
+++ app/businesses/availability_checker.rb
@@ -2,4 +2,8 @@ class AvailabilityChecker
   def self.lesson_available?(lesson)
     lesson.launch_datetime > Time.zone.now ? true : false
   end
+
+  def self.event_available?(event)
+    event.date > Time.zone.now ? true : false
+  end
 end
