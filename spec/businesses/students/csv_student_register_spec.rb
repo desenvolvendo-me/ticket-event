6f2425b45b14f95bb7bbc2a8f2d246bc86d9cb42
Cassio Santana <cassioo.roger@gmail.com>
2023-10-24 22:48:25 -0300

    test(business): Refactor CsvStudentRegister class test

--- spec/businesses/students/csv_student_register_spec.rb
+++ spec/businesses/students/csv_student_register_spec.rb
@@ -2,11 +2,25 @@ require 'rails_helper'
 
 RSpec.describe Students::CsvStudentRegister do
   let(:event) { create(:event) }
-  let(:csv_path) {'spec/support/leads_export.csv'}
-  let(:csv_student_register) {Students::CsvStudentRegister.new(event: event, csv_path: csv_path)}
+  let(:csv_path) { "spec/support/leads_export.csv" }
+  let!(:csv_data) { CSV.read(csv_path, headers: true).first }
 
-  it 'change image student' do
-    csv_student_register.call
+  subject(:csv_student_register) do
+    Students::CsvStudentRegister.call(event: event, csv_path: csv_path)
+  end
 
+  describe "#call" do
+    let(:last_student_created) { Student.last }
+
+    context "when the CSV data is successfully processed" do
+
+      it "increases the Student count and ensures the new Student info matches the CSV data" do
+
+        expect { csv_student_register }.to change(Student, :count).by(1)
+        expect(last_student_created.email).to eq(csv_data["email"])
+        expect(last_student_created.phone).to eq(csv_data["phone"])
+        expect(last_student_created.name).to eq(csv_data["fullname"])
+      end
+    end
   end
 end
