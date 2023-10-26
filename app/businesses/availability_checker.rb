c285e83157a301936eb20ef024496178cdd97913
Philipe Rodrigues <dev.philipe@outlook.com>
2023-10-26 16:52:25 -0300

    chore(I18N): translate checker error message

--- app/businesses/availability_checker.rb
+++
@@ -1,9 +0,0 @@
-class AvailabilityChecker
-  def self.lesson_available?(lesson)
-    lesson.launch_datetime > Time.zone.now ? true : false
-  end
-
-  def self.event_available?(event)
-    event.date > Time.zone.now ? true : false
-  end
-end
