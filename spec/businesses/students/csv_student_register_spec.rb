ebd5c1dccfc43243eb833bd23e0437ce4ca66797
Cassio Santana <cassioo.roger@gmail.com>
2023-10-25 23:44:07 -0300

    test(business): removes the event dependency from CsvStudentRegister and add assertions.

--- spec/businesses/students/csv_student_register_spec.rb
+++ spec/businesses/students/csv_student_register_spec.rb
@@ -1,12 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Students::CsvStudentRegister do
-  let(:event) { create(:event) }
   let(:csv_path) { "spec/support/leads_export.csv" }
   let!(:csv_data) { CSV.read(csv_path, headers: true).first }
 
   subject(:csv_student_register) do
-    Students::CsvStudentRegister.call(event: event, csv_path: csv_path)
+    Students::CsvStudentRegister.call(csv_path: csv_path)
   end
 
   describe "#call" do
@@ -20,6 +19,8 @@ RSpec.describe Students::CsvStudentRegister do
         expect(last_student_created.email).to eq(csv_data["email"])
         expect(last_student_created.phone).to eq(csv_data["phone"])
         expect(last_student_created.name).to eq(csv_data["fullname"])
+        expect(last_student_created.profile_social).to eq(csv_data["profile_social"])
+        expect(last_student_created.type_social).to eq(csv_data["type_social"])
       end
     end
   end
