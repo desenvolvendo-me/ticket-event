359cdb7d486c53934ef8606defd3da079b3a8324
Philipe Rodrigues <dev.philipe@outlook.com>
2023-10-26 21:26:26 -0300

    chore(access::checker): create a TODO on private method lesson_available?

--- app/businesses/access/checker.rb
+++ app/businesses/access/checker.rb
@@ -21,6 +21,7 @@ module Access
     end
 
     def lesson_available?
+      # TODO: Refatorar para que retorne de forma mais inteligente, sendo assim retornará um hash com 2 opções, ou o link de acesso ou a data de lançamento da aula dependendo da condicional. Ex: "Acessar Ingresso", search_ticket_path
       @resource.launch_datetime > Time.zone.now
     end
 
