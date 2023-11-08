7864f0491289b3f98abdb88218f57e262491550a
Philipe <dev.philipe@outlook.com>
2023-11-08 10:53:55 -0300

    test: attempt to fix some tests

--- spec/businesses/quizzes/quiz_result_calculator_spec.rb
+++ spec/businesses/quizzes/quiz_result_calculator_spec.rb
@@ -15,9 +15,10 @@ RSpec.describe Quizzes::QuizResultCalculator do
                     "6"=>"answer1", "7"=>"answer1", "8"=>"answer3", "9"=>"answer4", "10"=>"answer2"}
 
       student = create(:student, phone: '2196604')
+      manager = create(:manager_user)
       event = create(:event)
       ticket = create(:ticket, student: student, event: event)
-      lesson = create(:lesson, event: event)
+      lesson = create(:lesson, event: event, manager_user: manager)
       quiz = create(:quiz, lesson: lesson)
       create(:quiz_question, correct_answer: 1, quiz: quiz)
       create(:quiz_question, correct_answer: 1, quiz: quiz)
@@ -42,9 +43,10 @@ RSpec.describe Quizzes::QuizResultCalculator do
       parameters = {"11"=>"answer3", "12"=>"answer3", "13"=>"answer1", "14"=>"answer4"}
 
       student = create(:student, phone: '2196604')
+      manager = create(:manager_user)
       event = create(:event)
       ticket = create(:ticket, student: student, event: event)
-      lesson = create(:lesson, event: event)
+      lesson = create(:lesson, event: event, manager_user: manager)
       quiz = create(:quiz, lesson: lesson)
       create(:quiz_question, correct_answer: 3, quiz: quiz)
       create(:quiz_question, correct_answer: 3, quiz: quiz)
@@ -63,9 +65,10 @@ RSpec.describe Quizzes::QuizResultCalculator do
       parameters = {"15"=>"answer1", "16"=>"answer2", "17"=>"answer3"}
 
       student = create(:student, phone: '2196604')
+      manager = create(:manager_user)
       event = create(:event)
       ticket = create(:ticket, student: student, event: event)
-      lesson = create(:lesson, event: event)
+      lesson = create(:lesson, event: event, manager_user: manager)
       quiz = create(:quiz, lesson: lesson)
       create(:quiz_question, correct_answer: 1, quiz: quiz)
       create(:quiz_question, correct_answer: 2, quiz: quiz)
