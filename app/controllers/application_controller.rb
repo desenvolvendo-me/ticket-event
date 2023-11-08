230072aadabe0dbec20e30c205342f6f48d2fbcd
Philipe <dev.philipe@outlook.com>
2023-11-08 09:53:32 -0300

    feat(applicationController): set_current_tenant before_action due to documentation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   include ActiveStorage::SetCurrent
+  set_current_tenant_through_filter
+  before_action :find_current_tenant
+
+  def find_current_tenant
+    set_current_tenant(current_manager_user)
+  end
 end
