6d2b1829864f7680af566e2674f38be43abcea07
Philipe <dev.philipe@outlook.com>
2023-11-13 21:01:53 -0300

    feat(devise): permit avatar, full_name and login parameters to manager_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   include ActiveStorage::SetCurrent
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :avatar_url, :full_name, :login, :password, :password_confirmation])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :login, :password])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:login, :full_name, :email, :password, :password_confirmation, :current_password, :avatar_url])
+  end
 end
