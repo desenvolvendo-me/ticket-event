dcb18742463640c848c1071341e6b2779d68f3b9
victoroliveira59 <victorsamueloliveira59@gmail.com>
2023-12-27 20:27:46 -0300

    chore(model ): Removed the association of ticket in prize draw

--- app/models/prize_draw.rb
+++ app/models/prize_draw.rb
@@ -10,20 +10,9 @@
 #  created_at    :datetime         not null
 #  updated_at    :datetime         not null
 #  event_id      :integer          not null
-#  ticket_id     :bigint           not null
-#
-# Indexes
-#
-#  index_prize_draws_on_ticket_id  (ticket_id)
-#
-# Foreign Keys
-#
-#  fk_rails_...  (ticket_id => tickets.id)
 #
 class PrizeDraw < ApplicationRecord
-  has_one :ticket, dependent: :destroy
   belongs_to :event
-  accepts_nested_attributes_for :ticket
 
   validates :name, :date, :prize, presence: :true
 end
