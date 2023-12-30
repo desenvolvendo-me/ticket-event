aa22019e43eaef41447846925e9430aba4e5276a
victoroliveira59 <victorsamueloliveira59@gmail.com>
2023-12-30 16:19:50 -0300

    refactor(controller): Added condition unless to controllers

--- app/controllers/manager/prize_draws_controller.rb
+++ app/controllers/manager/prize_draws_controller.rb
@@ -12,43 +12,32 @@ class Manager::PrizeDrawsController < ApplicationController
 
   def new
     @prize_draw = PrizeDraw.new
-
   end
 
   def create
     @prize_draw = @event.create_prize_draw(prize_draw_params)
-
-    if @prize_draw.save
+    render :new unless @prize_draw.save
       redirect_to manager_event_prize_draws_path(event_id: @event, id: @prize_draw)
-    else
-      render :new
-    end
   end
 
 
   def edit; end
 
   def update
-    if @prize_draw.update(prize_draw_params)
-      redirect_to manager_event_prize_draw_path(@prize_draw)
-    else
-      render :edit
-    end
+    render :edit unless @prize_draw.update(prize_draw_params)
+    redirect_to manager_event_prize_draw_path(@prize_draw)
   end
 
   def destroy
-    if @prize_draw.destroy
-      redirect_to manager_event_prize_draws_path
-    end
+     @prize_draw.destroy
+     redirect_to manager_event_prize_draws_path
   end
 
   private
 
   def set_event
     @event = Event.find_by(id: params[:event_id])
-
   end
-
 
   def set_prize_draw
     @prize_draw = PrizeDraw.find_by(id: params[:id])
