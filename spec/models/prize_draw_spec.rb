73f13d152670bafd48c52a06fc6484f9610a7cdc
victoroliveira59 <victorsamueloliveira59@gmail.com>
2023-12-30 08:19:16 -0300

    test(model): "Added tests for validations in the Prize Draw model."

---
+++ spec/models/prize_draw_spec.rb
@@ -0,0 +1,21 @@
+# == Schema Information
+#
+# Table name: prize_draws
+#
+#  id            :bigint           not null, primary key
+#  date          :datetime
+#  name          :string
+#  prize         :string
+#  winner_ticket :string
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  event_id      :bigint           not null
+#
+require 'rails_helper'
+
+RSpec.describe PrizeDraw, type: :model do
+  it { should belong_to(:event)}
+  it { should validate_presence_of(:name)}
+  it { should validate_presence_of(:date)}
+  it { should validate_presence_of(:prize)}
+end
