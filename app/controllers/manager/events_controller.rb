58202e1bb9047c61aa52e5d7277ca77192b53bad
Hegnon <hegnondutra@gmail.com>
2024-01-02 22:14:33 -0300

    feat: add require duration

--- app/controllers/manager/events_controller.rb
+++ app/controllers/manager/events_controller.rb
@@ -49,6 +49,6 @@ class Manager::EventsController < ApplicationController
   end
 
   def event_params
-    params.require(:event).permit(:name, :description, :date, :launch, :community_link, :purchase_link, :purchase_date, :image)
+    params.require(:event).permit(:name, :description, :date, :launch, :community_link, :purchase_link, :purchase_date, :image, :duration)
   end
 end
