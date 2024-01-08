3cad4d04b12443a71f324b5a7f8e33b0f1c35938
victoroiveira59 <victorsamueloliveira59@gmail.com>
2024-01-08 16:27:54 -0300

    chore(factory): Update factory prize_draw

--- spec/factories/prize_draws.rb
+++ spec/factories/prize_draws.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: prize_draws
+#
+#  id         :bigint           not null, primary key
+#  date       :datetime
+#  name       :string
+#  prize      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  event_id   :bigint           not null
+#
 FactoryBot.define do
   factory :prize_draw do
     name { 'Bootcamp' }
