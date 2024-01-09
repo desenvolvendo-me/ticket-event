c97b1cd7f5dec6bb08dac41f52cb775b543cb929
victoroliveira59 <victorsamueloliveira59@gmail.com>
2024-01-09 05:34:06 -0300

    build(model): "Included foreign key ticket for the prize draw."

--- app/models/prize_draw.rb
+++ app/models/prize_draw.rb
@@ -10,6 +10,15 @@
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #  event_id   :bigint           not null
+#  ticket_id  :bigint
+#
+# Indexes
+#
+#  index_prize_draws_on_ticket_id  (ticket_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (ticket_id => tickets.id)
 #
 class PrizeDraw < ApplicationRecord
   belongs_to :event
