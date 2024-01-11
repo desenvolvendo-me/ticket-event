ed5fb539036815f949608286f99a92ba55ed1370
victoroliveira59 <victorsamueloliveira59@gmail.com>
2024-01-11 20:19:30 -0300

    test(generator): Realized changes in test for accept

--- spec/businesses/prize_draws/generator_spec.rb
+++ spec/businesses/prize_draws/generator_spec.rb
@@ -4,25 +4,24 @@ RSpec.describe PrizeDraws::Generator do
   let(:event) { create(:event) }
   let(:prize_draw) { create(:prize_draw, event: event) }
   let(:ticket) { create(:ticket, event: event) }
-
+  let(:ticket) { create(:ticket, prize_draw: prize_draw) }
+  let(:prize_draw) { create(:prize_draw)}
   context '#call' do
     it 'draws a ticket and creates a winner ticket' do
       student = create(:student)
-      eligible_ticket = create(:ticket, event: event, student: student, student_score: 70)
+      drawn_ticket  = create(:ticket, event: event, student: student, student_score: 70, prize_draw: prize_draw)
 
-      allow(event.tickets).to receive(:where).and_return([eligible_ticket])
-      allow(event.tickets).to receive(:sample).and_return(eligible_ticket)
+      allow(event.tickets).to receive(:where).and_return([drawn_ticket ])
+      allow(event.tickets).to receive(:sample).and_return(drawn_ticket )
 
-      drawn_winner_ticket = PrizeDraws::Generator.call(event, prize_draw)
+      PrizeDraws::Generator.call(event, prize_draw)
 
       expect(WinnerTicket.count).to eq(1)
       expect(WinnerTicket.first.prize_draw).to eq(prize_draw)
-      expect(WinnerTicket.first.ticket).to eq(eligible_ticket)
+      expect(WinnerTicket.first.ticket).to eq(drawn_ticket )
       expect(WinnerTicket.first.winner).to eq(student.name)
       expect(WinnerTicket.first.prize_draw.id).to eq(prize_draw.id)
-      expect(WinnerTicket.first.ticket.student.name).to eq(student.name)
-
-
+      expect(WinnerTicket.first.ticket.student.name).to eq("Marco Castro")
     end
 
     it 'does not create a winner ticket if no eligible ticket is drawn' do
