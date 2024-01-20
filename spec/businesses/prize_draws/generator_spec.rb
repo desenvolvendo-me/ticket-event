a5707afd68c26fe05b947144cd711b60c74bdbb4
victoroliveira59 <victorsamueloliveira59@gmail.com>
2024-01-19 22:34:25 -0300

    test(generate): modified test generate

--- spec/businesses/prize_draws/generator_spec.rb
+++ spec/businesses/prize_draws/generator_spec.rb
@@ -4,19 +4,24 @@ RSpec.describe PrizeDraws::Generator do
   let(:event) { create(:event) }
   let(:prize_draw) { create(:prize_draw, event: event) }
   let(:ticket) { create(:ticket, event: event) }
-
+  let(:students) do
+    Array.new(5) do
+      create(:student, name: FFaker::Name.name)
+    end
+  end
 
   context '#call' do
     it 'draws a ticket and creates a winner ticket' do
-      student = create(:student)
-      drawn_ticket  = create(:ticket, event: event, student: student, student_score: 70, prize_draw: prize_draw)
+
+      drawn_ticket  = create(:ticket, event: event, prize_draw: prize_draw)
 
       allow(event.tickets).to receive(:where).and_return([drawn_ticket ])
       allow(event.tickets).to receive(:sample).and_return(drawn_ticket )
 
       PrizeDraws::Generator.call(event, prize_draw)
 
-      expect(drawn_ticket.student_id).to eq(student.id)
+      expect(drawn_ticket.student).to be_instance_of(Student)
+      expect(drawn_ticket.prize_draw).to be_instance_of(PrizeDraw)
       expect(drawn_ticket.event_id).to eq(event.id)
       expect(drawn_ticket).to be_instance_of(Ticket)
 
