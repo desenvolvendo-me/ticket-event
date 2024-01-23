fa211dc7962c93a543d8162b271a7ce8b032886c
victoroliveira59 <victorsamueloliveira59@gmail.com>
2024-01-23 05:46:37 -0300

    chore(controller):Changes in controller

--- app/controllers/manager/prize_draws_controller.rb
+++ app/controllers/manager/prize_draws_controller.rb
@@ -1,63 +1,66 @@
-class Manager::PrizeDrawsController < ApplicationController
-  before_action :set_prize_draw, only: %i[ index show new edit create update destroy prize_draw_winner]
-  before_action :set_event
+# frozen_string_literal: true
 
-  def index
-    @prize_draws = PrizeDraw.all
-  end
+module Manager
+  class PrizeDrawsController < ApplicationController
+    before_action :set_prize_draw, only: %i[index show new edit create update destroy prize_draw_winner]
+    before_action :set_event
 
-  def show
-    @prize_draws = PrizeDraw.find_by(id: params[:id])
-  end
+    def index
+      @prize_draws = PrizeDraw.all
+    end
 
-  def new
-    @prize_draw = PrizeDraw.new
-  end
+    def show
+      @prize_draws = PrizeDraw.find_by(id: params[:id])
+    end
 
-  def create
-    @prize_draw = @event.create_prize_draw(prize_draw_params)
-    render :new unless @prize_draw.save
+    def new
+      @prize_draw = PrizeDraw.new
+    end
 
-    redirect_to manager_event_prize_draws_path(event_id: @event, id: @prize_draw)
-  end
+    def create
+      @prize_draw = @event.create_prize_draw(prize_draw_params)
+      render :new unless @prize_draw.save
 
+      redirect_to manager_event_prize_draws_path(event_id: @event, id: @prize_draw)
+    end
 
-  def edit; end
+    def edit; end
 
-  def update
-    render :edit unless @prize_draw.update(prize_draw_params)
-    redirect_to manager_event_prize_draw_path(@event, @prize_draw)
-  end
+    def update
+      render :edit unless @prize_draw.update(prize_draw_params)
+      redirect_to manager_event_prize_draw_path(@event, @prize_draw)
+    end
 
-  def destroy
-     @prize_draw.destroy
-     redirect_to manager_event_prize_draws_path
-  end
-
-  def prize_draw_winner
-    @prize_draw = PrizeDraw.find(params[:id])
-    @ticket = PrizeDraws::Generator.new(@event, @prize_draw).call
-
-    if @ticket.present? && @prize_draw.present?
-      @prize_draw.update(winner: @ticket.student.name, ticket_email: @ticket.student.email, number_ticket: @ticket.number)
-      WinnerDrawToTicketMailer.with(prize_draw: PrizeDraw.last).send_winner_draw.deliver_now
-      render :prize_draw_winner
-    else
+    def destroy
+      @prize_draw.destroy
       redirect_to manager_event_prize_draws_path
     end
-  end
 
-  private
+    def prize_draw_winner
+      @prize_draw = PrizeDraw.find(params[:id])
+      @ticket = Ticket.find(params[:id])
+      @ticket = PrizeDraws::Generator.new(@event, @prize_draw).call
 
-  def set_event
-    @event = Event.find(params[:event_id])
-  end
+      if @ticket.present? && @prize_draw.present?
+        @prize_draw.update(ticket_id: @ticket)
+        render :prize_draw_winner
+      else
+        redirect_to manager_event_prize_draws_path
+      end
+    end
 
-  def set_prize_draw
-    @prize_draw = PrizeDraw.find_by(id: params[:id])
-  end
+    private
 
-  def prize_draw_params
-    params.require(:prize_draw).permit(:name, :date, :prize, :event_id, :winner, :ticket_email)
+    def set_event
+      @event = Event.find(params[:event_id])
+    end
+
+    def set_prize_draw
+      @prize_draw = PrizeDraw.find_by(id: params[:id])
+    end
+
+    def prize_draw_params
+      params.require(:prize_draw).permit(:name, :date, :prize, :event_id, :ticket_id)
+    end
   end
 end
