675375e18641ab75dfc5272b33cc23ff09575dd3
victoroliveira59 <victorsamueloliveira59@gmail.com>
2024-01-29 20:26:46 -0300

    chore(models): Changes in models prize draw

--- app/models/prize_draw.rb
+++ app/models/prize_draw.rb
@@ -13,10 +13,12 @@
 #
 # Indexes
 #
+#  index_prize_draws_on_event_id   (event_id)
 #  index_prize_draws_on_ticket_id  (ticket_id)
 #
 # Foreign Keys
 #
+#  fk_rails_...  (event_id => events.id)
 #  fk_rails_...  (ticket_id => tickets.id)
 #
 class PrizeDraw < ApplicationRecord
