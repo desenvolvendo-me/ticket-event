2365c37a0fff7ff9694d667e9630776b2e26d81a
Sergio Oseko <osk.sergio@gmail.com>
2024-02-18 18:25:58 -0300

    docs: removidos alguns comentários (id do sorteio (prize_draw_id)

--- spec/factories/tickets.rb
+++ spec/factories/tickets.rb
@@ -11,19 +11,16 @@
 #  created_at      :datetime         not null
 #  updated_at      :datetime         not null
 #  event_id        :bigint           not null
-#  prize_draw_id   :bigint
 #  student_id      :bigint           not null
 #
 # Indexes
 #
-#  index_tickets_on_event_id       (event_id)
-#  index_tickets_on_prize_draw_id  (prize_draw_id)
-#  index_tickets_on_student_id     (student_id)
+#  index_tickets_on_event_id    (event_id)
+#  index_tickets_on_student_id  (student_id)
 #
 # Foreign Keys
 #
 #  fk_rails_...  (event_id => events.id)
-#  fk_rails_...  (prize_draw_id => prize_draws.id)
 #  fk_rails_...  (student_id => students.id)
 #
 FactoryBot.define do
