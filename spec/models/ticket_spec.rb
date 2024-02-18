ec3e2460bdb775a3d21113e803fb914893470d6e
Sergio Oseko <osk.sergio@gmail.com>
2024-02-18 18:18:49 -0300

    test: removido a verificação do sorteio

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -11,19 +11,16 @@
 #  created_at      :datetime         not null
 #  updated_at      :datetime         not null
 #  event_id        :bigint           not null
-#  prize_draw_id   :bigint
 #  student_id      :bigint           not null
 #
 # Indexes
 #
-#  index_tickets_on_event_id       (event_id)
-#  index_tickets_on_prize_draw_id  (prize_draw_id)
-#  index_tickets_on_student_id     (student_id)
+#  index_tickets_on_event_id    (event_id)
+#  index_tickets_on_student_id  (student_id)
 #
 # Foreign Keys
 #
 #  fk_rails_...  (event_id => events.id)
-#  fk_rails_...  (prize_draw_id => prize_draws.id)
 #  fk_rails_...  (student_id => students.id)
 #
 require 'rails_helper'
@@ -31,7 +28,6 @@ require 'rails_helper'
 RSpec.describe Ticket, type: :model do
   it { should belong_to(:event)}
   it { should belong_to(:student)}
-  it { should have_one(:prize_draw)}
   it { should have_one_attached(:svg)}
   it { should have_one_attached(:png)}
 end
