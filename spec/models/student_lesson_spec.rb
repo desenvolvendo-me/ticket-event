a4b331df9549b2398d1a9003634f9a5d3f713c6f
eskokado <eskokado@gmail.com>
2024-02-29 15:15:47 -0300

    test(student_lesson): relation with lesson and student

--- spec/models/student_lesson_spec.rb
+++ spec/models/student_lesson_spec.rb
@@ -22,5 +22,6 @@
 require 'rails_helper'
 
 RSpec.describe StudentLesson, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:lesson) }
+  it { should belong_to(:student) }
 end
