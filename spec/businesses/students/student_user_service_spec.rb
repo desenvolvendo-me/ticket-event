05527489678c8f120ba046af63195819966a848a
eskokado <eskokado@gmail.com>
2024-03-01 13:10:26 -0300

    test(create_associated_student): when a student with the same email already exists then does not create a new associated student

--- spec/businesses/students/student_user_service_spec.rb
+++ spec/businesses/students/student_user_service_spec.rb
@@ -16,5 +16,18 @@ RSpec.describe Students::StudentUserService, type: :service do
         expect(student_user.student).to be_present
       end
     end
+
+    context 'when a student with the same email already exists' do
+      before do
+        create(:student, email: student_user.email)
+      end
+
+      it 'does not create a new associated student' do
+        expect {
+          described_class.create_associated_student(student_user)
+        }.not_to change(Student, :count)
+      end
+    end
+
   end
 end
