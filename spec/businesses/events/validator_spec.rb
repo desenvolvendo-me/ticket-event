806e5b326e6625882ee961ccf89014831333d12e
eskokado <eskokado@gmail.com>
2024-03-13 10:52:00 -0300

    test(events::validator): #call when duration format is valid does not add an error message for duration

--- spec/businesses/events/validator_spec.rb
+++ spec/businesses/events/validator_spec.rb
@@ -16,5 +16,18 @@ RSpec.describe Events::Validator do
         expect(subject.errors).to include({ duration: "Obrigatório informar a duração do evento" })
       end
     end
+
+    context "when duration format is valid" do
+      before do
+        allow(event).to receive(:duration).and_return(2)
+        allow(event).to receive(:is_visible_after_time?).and_return(true)
+        allow(event).to receive(:visible_after_time).and_return(nil)
+        subject.call
+      end
+
+      it "does not add an error message for duration" do
+        expect(subject.errors).not_to include({ duration: "Invalid duration format" })
+      end
+    end
   end
 end
