1d1149d38e819e24057a751821316acb23b839db
eskokado <eskokado@gmail.com>
2024-03-13 07:04:22 -0300

    refactor(event): impl i18n validator duration and after_time

--- app/businesses/events/validator.rb
+++ app/businesses/events/validator.rb
@@ -17,14 +17,14 @@ module Events
 
     def validate_duration_format
       unless @event.duration.is_a?(Integer) && @event.duration.positive?
-        @errors << { duration: :invalid }
+        @errors << { duration: I18n.t("businesses.event.validator.duration") }
       end
     end
 
     def validate_visibility_fields
       if @event.is_visible_after_time?
         if @event.visible_after_time.blank?
-          @errors << { visible_after_time: :blank }
+          @errors << { visible_after_time: I18n.t("businesses.event.validator.after_time") }
         end
       end
     end
