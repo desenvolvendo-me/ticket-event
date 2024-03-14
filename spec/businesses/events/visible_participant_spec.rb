99a580200870d533b6bdb873026f503115366bab
eskokado <eskokado@gmail.com>
2024-03-14 12:01:17 -0300

    test(events): #call business visible_participant when the event is visible to registered participants when current_student_user is present returns true

---
+++ spec/businesses/events/visible_participant_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe Events::VisibleParticipant, type: :business_application do
+  describe '#call' do
+    let(:event) { double("Event", is_visible_to_registered_participants: is_visible_to_registered_participants) }
+
+    context 'when the event is visible to registered participants' do
+      let(:is_visible_to_registered_participants) { true }
+
+      context 'when current_student_user is present' do
+        let(:current_student_user) { double("User") }
+        let(:visible_participant) { Events::VisibleParticipant.new(event, current_student_user) }
+
+        it 'returns true' do
+          expect(visible_participant.call).to eq(true)
+        end
+      end
+    end
+  end
+end
